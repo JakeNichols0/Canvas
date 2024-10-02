709e17649948a34be28d9197f14a3a3d87deb3dd
Jake Nichols <adages.shows_03@icloud.com>
2024-10-02 08:10:25 -0500

    Apative Class Names Made the class name displayed match the actual name of the class Made modules icon bigger when you click on them

--- content.js
+++ content.js
@@ -1,19 +1,20 @@
 let cont = document.getElementById("course_home_content");
+let className = document.getElementsByClassName("ic-Action-header")[0].textContent;
 let html = `
 <div style="width: 100%;height: 25vh;background-image: url('https://hls.harvard.edu/wp-content/uploads/2022/05/CONSTITUTION_iStock-923052552_2500.jpg');background-repeat: no-repeat;background-size: cover;">
-    <h1 class="classNameH1">AP Computer Science</h1>
+    <h1 class="classNameH1">${className}</h1>
 </div>
 <table style="width: 100%;margin-top: 50px;">
   <tbody>
     <tr>
       <td style="width: 50%;">
-        <a href="https://aisdblend.instructure.com/courses/541113/modules">
+        <a href="${window.location.href}/modules">
           <div class="navigationDivBackground" style="background-image: url('https://cdn.britannica.com/69/75569-050-7AB67C4B/herm-Socrates-half-original-Greek-Capitoline-Museums.jpg');"></div>
           <h2 style="text-align: center;">Modules</h2>
         </a>
       </td>
       <td style="width: 50%;">
-        <a href="https://aisdblend.instructure.com/courses/541113/modules">
+        <a href="${window.location.href}/syllabus">
           <div class="navigationDivBackground" style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1200px-Colosseo_2020.jpg');"></div>
           <h2 style="text-align: center;">Syllabus</h2>
         </a>
@@ -23,9 +24,13 @@ let html = `
 </table>`
 
 window.onload = function() {
-    chrome.storage.sync.get('on', function(data) {
-        if (data.on) {
-            cont.lastElementChild.insertAdjacentHTML("afterend", html);
+  chrome.storage.sync.get('on', function(data) {
+      if (data.on) {
+        if (cont.children[0].id == 'wiki_page_show') {
+          cont.children[0].firstElementChild.insertAdjacentHTML("afterend", html);
+        } else {
+          cont.lastElementChild.insertAdjacentHTML("afterend", html);
         }
-    });
+      }
+  });
 }
